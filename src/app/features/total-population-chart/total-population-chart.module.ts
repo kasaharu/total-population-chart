e0b37806b605d2397e5bf8de6c68a0e8f0585034
Wataru.Kasahara <kasahara1019@gmail.com>
2022-04-28 22:38:02 +0900

    yarn ng g module features/total-population-chart --routing

---
+++ src/app/features/total-population-chart/total-population-chart.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TotalPopulationChartRoutingModule } from './total-population-chart-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TotalPopulationChartRoutingModule
+  ]
+})
+export class TotalPopulationChartModule { }
