73622fbeb18cfe627593f19a58fcf50a67014067
Wataru.Kasahara <kasahara1019@gmail.com>
2022-04-28 22:42:29 +0900

    feat: / にアクセスしたときに TotalPopulationChartComponent を表示するようにルーティングを設定

--- src/app/features/total-population-chart/total-population-chart-routing.module.ts
+++ src/app/features/total-population-chart/total-population-chart-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TotalPopulationChartComponent } from './pages/total-population-chart/total-population-chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: TotalPopulationChartComponent, pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
