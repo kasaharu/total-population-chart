cf20abcee48b6c04c5dd1a91d4d5569c7798b6a3
Wataru.Kasahara <kasahara1019@gmail.com>
2022-04-29 13:36:17 +0900

    feat: 都道府県一覧を取得する api method を実装

--- src/app/infrastructures/api/prefecture.api.spec.ts
+++ src/app/infrastructures/api/prefecture.api.spec.ts
@@ -1,16 +1,38 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { Prefecture } from '../../domain/prefecture';
+import { environment } from '../../../environments/environment';
 import { PrefectureApi } from './prefecture.api';
 
 describe('PrefectureApi', () => {
   let api: PrefectureApi;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
     api = TestBed.inject(PrefectureApi);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(api).toBeTruthy();
+  });
+
+  it('getPrefectures()', () => {
+    const prefectures: Prefecture[] = [
+      { prefCode: 1, prefName: '北海道' },
+      { prefCode: 2, prefName: '沖縄県' },
+    ];
+    const response = {
+      result: prefectures,
+    };
+
+    api.getPrefectures().subscribe((resp) => {
+      expect(resp).toEqual(prefectures);
+    });
+
+    const req = httpTestingController.expectOne(`${environment.apiEndpoint}/api/v1/prefectures`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(response);
   });
 });
--- src/app/infrastructures/api/prefecture.api.ts
+++ src/app/infrastructures/api/prefecture.api.ts
@@ -1,8 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { Prefecture } from '../../domain/prefecture';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'X-API-KEY': environment.apiKey,
+  }),
+};
+
+interface ResponseType {
+  message: string | null;
+  result: Prefecture[];
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class PrefectureApi {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getPrefectures(): Observable<Prefecture[]> {
+    return this.http.get<ResponseType>(`${environment.apiEndpoint}/api/v1/prefectures`, httpOptions).pipe(map((res) => res.result));
+  }
 }
