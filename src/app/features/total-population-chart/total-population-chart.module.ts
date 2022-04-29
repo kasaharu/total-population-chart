968676537bc66d76f9dd309075ada272e24631c6
Wataru.Kasahara <kasahara1019@gmail.com>
2022-04-29 18:25:24 +0900

    feat: 都道府県一覧を ReactiveForms でフォームに変更

--- src/app/features/total-population-chart/total-population-chart.module.ts
+++ src/app/features/total-population-chart/total-population-chart.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { TotalPopulationChartRoutingModule } from './total-population-chart-routing.module';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TotalPopulationChartComponent } from './pages/total-population-chart/total-population-chart.component';
+import { TotalPopulationChartRoutingModule } from './total-population-chart-routing.module';
 import { LyTotalPopulationChartComponent } from './views/ly-total-population-chart/ly-total-population-chart.component';
 import { PrefecturesFormComponent } from './views/prefectures-form/prefectures-form.component';
 
 @NgModule({
   declarations: [TotalPopulationChartComponent, LyTotalPopulationChartComponent, PrefecturesFormComponent],
-  imports: [CommonModule, TotalPopulationChartRoutingModule],
+  imports: [CommonModule, TotalPopulationChartRoutingModule, ReactiveFormsModule],
 })
 export class TotalPopulationChartModule {}
--- src/app/features/total-population-chart/views/prefectures-form/prefectures-form.component.ts
+++ src/app/features/total-population-chart/views/prefectures-form/prefectures-form.component.ts
@@ -1,12 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { Prefecture } from '../../../../domain/prefecture';
 
 @Component({
   selector: 'app-prefectures-form',
   templateUrl: './prefectures-form.component.html',
   styleUrls: ['./prefectures-form.component.scss'],
 })
-export class PrefecturesFormComponent implements OnInit {
+export class PrefecturesFormComponent implements OnChanges {
   constructor() {}
+  @Input() prefectures: Prefecture[] | null = null;
 
-  ngOnInit(): void {}
+  formGroup = new FormGroup({ checkboxes: new FormArray([]) });
+
+  ngOnChanges(): void {
+    if (this.prefectures !== null) {
+      const prefecturesFormControl = this.prefectures.map((_) => new FormControl(false));
+      this.formGroup = new FormGroup({
+        checkboxes: new FormArray(prefecturesFormControl),
+      });
+    }
+  }
+
+  get checkboxes() {
+    return this.formGroup.get('checkboxes') as FormArray;
+  }
+
+  getPrefectureName(i: number) {
+    return this.prefectures === null ? '' : this.prefectures[i].prefName;
+  }
+
+  onChange(): void {
+    const checkboxes: boolean[] = this.formGroup.value.checkboxes;
+
+    const checkedBox: Prefecture[] = [];
+    checkboxes.forEach((item, index: number) => {
+      if (this.prefectures === null) {
+        return;
+      }
+
+      if (item === true) {
+        checkedBox.push(this.prefectures[index]);
+      }
+    });
+  }
 }
