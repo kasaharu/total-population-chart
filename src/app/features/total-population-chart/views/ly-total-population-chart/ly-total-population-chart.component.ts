2a8b7b294a8ea1e27dd8d5f1ef2da0551e966759
Wataru.Kasahara <kasahara1019@gmail.com>
2022-04-29 15:07:59 +0900

    feat: 都道府県一覧を表示

--- src/app/features/total-population-chart/views/ly-total-population-chart/ly-total-population-chart.component.ts
+++ src/app/features/total-population-chart/views/ly-total-population-chart/ly-total-population-chart.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Prefecture } from '../../../../domain/prefecture';
 
 @Component({
   selector: 'app-ly-total-population-chart',
   templateUrl: './ly-total-population-chart.component.html',
   styleUrls: ['./ly-total-population-chart.component.scss'],
 })
-export class LyTotalPopulationChartComponent implements OnInit {
+export class LyTotalPopulationChartComponent {
   constructor() {}
 
-  ngOnInit(): void {}
+  @Input() prefectures: Prefecture[] | null = null;
 }
