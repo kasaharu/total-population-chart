a785f439e6a2a83a561eb7fb042e78b33f0d73fd
Wataru.Kasahara <kasahara1019@gmail.com>
2022-04-29 12:59:22 +0900

    fix: @angular-eslint/no-empty-lifecycle-method のルールを一時的に無効に

--- src/app/features/total-population-chart/pages/total-population-chart/total-population-chart.component.ts
+++ src/app/features/total-population-chart/pages/total-population-chart/total-population-chart.component.ts
@@ -8,5 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class TotalPopulationChartComponent implements OnInit {
   constructor() {}
 
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit(): void {}
 }
