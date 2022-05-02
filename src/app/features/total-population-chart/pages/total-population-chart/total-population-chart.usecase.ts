c998d424b1ad17e3681a17036eef63827e45addd
Wataru.Kasahara <kasahara1019@gmail.com>
2022-05-02 10:31:25 +0900

    feat: ComponentStore に総人口情報として populationComposition を追加

--- src/app/features/total-population-chart/pages/total-population-chart/total-population-chart.usecase.ts
+++ src/app/features/total-population-chart/pages/total-population-chart/total-population-chart.usecase.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
 import { lastValueFrom } from 'rxjs';
-import { PrefectureApi } from '../../../../infrastructures/api/prefecture.api';
+import { PopulationComposition } from '../../../../domain/population-composition';
 import { Prefecture } from '../../../../domain/prefecture';
+import { PrefectureApi } from '../../../../infrastructures/api/prefecture.api';
 
 export interface TotalPopulationChartState {
   prefectures: Prefecture[] | null;
+  populationComposition: PopulationComposition | null;
 }
 
 @Injectable()
 export class TotalPopulationChartUsecase extends ComponentStore<TotalPopulationChartState> {
   constructor(private _prefectureApi: PrefectureApi) {
-    super({ prefectures: null });
+    super({ prefectures: null, populationComposition: null });
   }
 
   readonly prefectures$ = this.select((state) => state.prefectures);
-  readonly savePrefectures = this.updater((_, prefectures: Prefecture[]) => ({ prefectures }));
+  readonly savePrefectures = this.updater((state, prefectures: Prefecture[]) => ({ ...state, prefectures }));
+  readonly populationComposition$ = this.select((state) => state.populationComposition);
+  readonly savePopulationComposition = this.updater((state, populationComposition: PopulationComposition) => ({
+    ...state,
+    populationComposition,
+  }));
 
   async fetchPrefectures(): Promise<void> {
     const prefectures = await lastValueFrom(this._prefectureApi.getPrefectures());
