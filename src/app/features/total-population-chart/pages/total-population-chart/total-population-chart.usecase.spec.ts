364eddef91d476e4f2a1e5713a835febce0f6fd1
Wataru.Kasahara <kasahara1019@gmail.com>
2022-05-17 08:06:30 +0900

    test: TotalPopulationChartUsecase#fetchPrefectures のテストケース追加

--- src/app/features/total-population-chart/pages/total-population-chart/total-population-chart.usecase.spec.ts
+++ src/app/features/total-population-chart/pages/total-population-chart/total-population-chart.usecase.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Prefecture } from '../../../../domain/prefecture';
 import { PopulationCompositionApi } from '../../../../infrastructures/api/population-composition.api';
 import { PrefectureApi } from '../../../../infrastructures/api/prefecture.api';
 import { TotalPopulationChartUsecase } from './total-population-chart.usecase';
@@ -13,6 +15,8 @@ class MockPopulationCompositionApi implements Partial<PopulationCompositionApi> 
 
 describe('TotalPopulationChartUsecase', () => {
   let usecase: TotalPopulationChartUsecase;
+  let prefectureApi: PrefectureApi;
+  let populationCompositionApi: PopulationCompositionApi;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -23,9 +27,23 @@ describe('TotalPopulationChartUsecase', () => {
       ],
     });
     usecase = TestBed.inject(TotalPopulationChartUsecase);
+    prefectureApi = TestBed.inject(PrefectureApi);
+    populationCompositionApi = TestBed.inject(PopulationCompositionApi);
   });
 
   it('should be created', () => {
     expect(usecase).toBeTruthy();
+  });
+
+  describe('fetchPrefectures()', () => {
+    it('usecase.savePrefectures() が呼ばれること', async () => {
+      const prefectures: Prefecture[] = [];
+      spyOn(prefectureApi, 'getPrefectures').and.returnValue(of(prefectures));
+      spyOn(usecase, 'savePrefectures');
+
+      await usecase.fetchPrefectures();
+
+      expect(usecase.savePrefectures).toHaveBeenCalledWith(prefectures);
+    });
   });
 });
