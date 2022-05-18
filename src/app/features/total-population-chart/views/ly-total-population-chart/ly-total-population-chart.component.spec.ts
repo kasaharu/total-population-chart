e5cd2272e001998bab225b283eb12e01265d616f
Wataru.Kasahara <kasahara1019@gmail.com>
2022-05-18 20:34:29 +0900

    test: LyTotalPopulationChartComponent#selectPref のテストケースを追加

--- src/app/features/total-population-chart/views/ly-total-population-chart/ly-total-population-chart.component.spec.ts
+++ src/app/features/total-population-chart/views/ly-total-population-chart/ly-total-population-chart.component.spec.ts
@@ -1,24 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { render } from '@testing-library/angular';
+import { Prefecture } from '../../../../domain/prefecture';
 import { LyTotalPopulationChartComponent } from './ly-total-population-chart.component';
 
 describe('LyTotalPopulationChartComponent', () => {
-  let component: LyTotalPopulationChartComponent;
-  let fixture: ComponentFixture<LyTotalPopulationChartComponent>;
+  describe('selectPref()', () => {
+    it('component.prefSelected.emit() が呼ばれること', async () => {
+      const { fixture } = await render(LyTotalPopulationChartComponent);
+      const component = fixture.componentInstance;
+      const prefectures: Prefecture[] = [{ prefCode: 1, prefName: '東京都' }];
+      spyOn(component.prefSelected, 'emit');
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LyTotalPopulationChartComponent],
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LyTotalPopulationChartComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+      component.selectPref(prefectures);
+      expect(component.prefSelected.emit).toHaveBeenCalledWith(prefectures);
+    });
   });
 });
