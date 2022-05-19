27d04a266fe36fd66c16317151b9371ce48d2105
Wataru.Kasahara <kasahara1019@gmail.com>
2022-05-19 20:28:44 +0900

    test: PrefecturesFormComponent の依存を解決

--- src/app/features/total-population-chart/views/prefectures-form/prefectures-form.component.spec.ts
+++ src/app/features/total-population-chart/views/prefectures-form/prefectures-form.component.spec.ts
@@ -1,9 +1,10 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { render } from '@testing-library/angular';
 import { PrefecturesFormComponent } from './prefectures-form.component';
 
 describe('PrefecturesFormComponent', () => {
   it('should create', async () => {
-    const { fixture } = await render(PrefecturesFormComponent);
+    const { fixture } = await render(PrefecturesFormComponent, { imports: [ReactiveFormsModule] });
     const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
