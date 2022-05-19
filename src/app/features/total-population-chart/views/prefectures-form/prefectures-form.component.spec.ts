68ce2235409944d528ee5fa70609bf15f67e0f43
Wataru.Kasahara <kasahara1019@gmail.com>
2022-05-19 20:52:02 +0900

    test: PrefecturesFormComponent 初期化のテストケースを追加

--- src/app/features/total-population-chart/views/prefectures-form/prefectures-form.component.spec.ts
+++ src/app/features/total-population-chart/views/prefectures-form/prefectures-form.component.spec.ts
@@ -1,11 +1,32 @@
 import { ReactiveFormsModule } from '@angular/forms';
 import { render } from '@testing-library/angular';
 import { PrefecturesFormComponent } from './prefectures-form.component';
+import { Prefecture } from '../../../../domain/prefecture';
 
 describe('PrefecturesFormComponent', () => {
   it('should create', async () => {
     const { fixture } = await render(PrefecturesFormComponent, { imports: [ReactiveFormsModule] });
     const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  describe('初期化', () => {
+    it('prefectures が null のとき formGroup の checkboxes は [] になること', async () => {
+      const prefectures = null;
+      const { fixture } = await render(PrefecturesFormComponent, { imports: [ReactiveFormsModule], componentProperties: { prefectures } });
+      const component = fixture.componentInstance;
+      expect(component.checkboxes.value).toEqual([]);
+    });
+
+    it('prefectures が null でないとき formGroup の checkboxes は prefectures と同じ長さの false の配列になること', async () => {
+      const prefectures: Prefecture[] = [
+        { prefCode: 1, prefName: '東京都' },
+        { prefCode: 2, prefName: '沖縄県' },
+      ];
+      const expected = [false, false];
+      const { fixture } = await render(PrefecturesFormComponent, { imports: [ReactiveFormsModule], componentProperties: { prefectures } });
+      const component = fixture.componentInstance;
+      expect(component.checkboxes.value).toEqual(expected);
+    });
   });
 });
